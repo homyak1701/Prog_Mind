d5e8162f363e72bfb8ce25a18f38aa231372b55b
andrey <Andrey Dolgikh>
2026-02-27 16:58:43 +0700

    06.02.27 16:58

--- BookS/1.1/2.1. Сортировка вставкой/2.1.3/code.c
+++ BookS/1.1/2.1. Сортировка вставкой/2.1.3/code.c
@@ -4,9 +4,10 @@ int main(int argc, char *argv[])
 {
     if (argc < 3)
     {
-        printf("argc < 3");
+        printf("Использование: %s <v> <a1> <a2> ... <an>\n", argv[0]);
         return 1;
     }
 
-    
+    int v = atoi (argv[1]);   
+
 }
