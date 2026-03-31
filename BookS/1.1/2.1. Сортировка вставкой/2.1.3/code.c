902b1c9014bf9157bc7497563e8312cb18b0e4a7
andrey <Andrey Dolgikh>
2026-03-31 17:58:47 +0700

    31.03.2026

--- BookS/1.1/2.1. Сортировка вставкой/2.1.3/code.c
+++ BookS/1.1/2.1. Сортировка вставкой/2.1.3/code.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char *argv[])
 {
@@ -8,6 +9,40 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-    int v = atoi (argv[1]);   
+    int v = atoi(argv[1]);   
+    int i = 0;
 
+    /*
+        Инвариант — это условие, которое остается истинным
+        на протяжении всего выполнения программы.
+
+        при i равным от 0 до (argc - 1)
+        atoi(argv[i - 1]) != v
+    */
+    while (i < argc)
+    {
+        if (i == 0)
+        {
+            printf("Инициализация: atoi(argv[i - 1]) = %d; v = %d", atoi(argv[i - 1]), v);
+        }
+        else
+        {
+            printf("Сохранение: atoi(argv[i - 1]) = %d; v = %d", atoi(argv[i - 1]), v);
+        }
+
+        if (atoi(argv[i - 1]) == v)
+        {
+            break;
+        }
+        else if (i == argc)
+        {
+            printf("Завершение: atoi(argv[i - 1]) = %d; v = %d", atoi(argv[i - 1]), v);
+            i = -1;
+            break;
+        }
+            
+        i++;
+    }
+
+    printf("Ответ i = %d", i);
 }
